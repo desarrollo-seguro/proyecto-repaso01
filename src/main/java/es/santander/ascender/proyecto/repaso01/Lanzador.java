4d1f0f3c87063e03c230048a83227bff6a371052
curso00 <curso00@nomail.com>
2024-12-04 18:19:38 +0100

    Ejemplo simple de estructura de carpetas, clases y llaves

---
+++ src/main/java/es/santander/ascender/proyecto/repaso01/Lanzador.java
@@ -0,0 +1,10 @@
+package es.santander.ascender.proyecto.repaso01;
+
+/**
+ * Hello world!
+ */
+public class Lanzador {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/es/santander/ascender/proyecto/repaso01/Terminator.java
@@ -0,0 +1,25 @@
+package es.santander.ascender.proyecto.repaso01;
+
+public class Terminator {
+    private int peso;
+    private String nombre;
+
+    public void disparar() {
+        // carga la escopeta
+        // apuntas
+        // apretar gatillo
+    }
+
+    {
+        }
+
+
+
+
+
+
+
+
+
+
+}
